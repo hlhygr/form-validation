044e794836a8fcb9d46f95bc4d72f164b64382ce
ygrhlh <15816230839@163.com>
2020-06-08 22:38:21 +0800

    优化index.js写法，将主要代码移入lib/validator.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-let reg = require('./lib/reg');  // 正则对象
+let _validator = require('./lib/validator');  // 引入验证器
 
-let validate = {};
-
-// 将正则对象注册到validate原型链上
-validate.reg = reg;
-
-/**
- * 验证方法
- * @param obj 要验证的字段对象 obj.k => 需验证字段 obj.v => 值
- * @return Boolean true or false
- * */
-validate.valid = (obj) => {
-    console.log(reg);
-};
-
-
-module.exports = validate;
+module.exports = _validator;
